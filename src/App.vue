22c9d0cabac9a627f781562c900a09f91effc65c
jpena146 <98130843+jpena146@users.noreply.github.com>
2025-02-14 21:44:03 -0600

    finished transaction components

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,12 @@
     import Balance from './components/Balance.vue';
     import IncomeExpense from './components/IncomeExpense.vue';
     import AddTransaction from './components/AddTransaction.vue';
+    import TransactionList from './components/TransactionList.vue';
     
     import {ref, computed} from 'vue'
 
     const transactions = ref([
-        {id:1, text:'Paycheck', amount: 700.00},
-        {id:2, text:'Water Bill', amount: -72.83},
-        {id:3, text:'Electric Bill', amount: -153.89},
-        {id:4, text:'Returened Item', amount: 20.00},
+
     ])
 
     const sum = computed(()=>{
@@ -36,6 +34,16 @@
         },0)
     })
 
+    const handleTransaction= (transactionData)=>{
+        transactions.value.push({
+            text: transactionData.text,
+            amount: transactionData.amount,
+        })
+    }
+
+    const handleDelete = (id) =>{
+        transactions.value= transactions.value.filter((x)=> x.id !== id)
+    }
 </script>
 
 
@@ -44,6 +52,8 @@
     <div class="container">
         <Balance :total="sum"></Balance>
         <IncomeExpense :income="moneyIn" :expense="moneyOut"></IncomeExpense>
-        <AddTransaction></AddTransaction>
+        <AddTransaction @transactionSubmitted="handleTransaction"></AddTransaction>
+        <TransactionList :transactions="transactions" @transactionDeleted="handleDelete"></TransactionList>
+
     </div>
 </template>
